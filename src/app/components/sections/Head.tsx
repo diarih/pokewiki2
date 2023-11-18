ef6f20af4bcb1c7614bfe6952ccd97e48b4098e7
diarihza <diarihza@gmail.com>
2023-11-18 14:02:55 +0700

    feat: added head section

---
+++ src/app/components/sections/Head.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Head() {
+    return (
+        <section className='flex justify-center flex-col items-center gap-4 mb-12'>
+            <div className='text-lg'>Welcome to</div>
+            <div className='md:text-8xl text-4xl font-black'>PokeWiki</div>
+        </section>
+    )
+}
